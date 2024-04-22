53cf12ad87e576ac2bfd8d7287bbc28ded7577cd
shakalac <123456789+odin@users.noreply.github.com>
2024-04-22 09:10:42 -0500

    Trying to commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Etch-A-Sketch
+An Etch-A-Sketch made out of divs where I utilize flexbox to create the grid. A "New Grid" button allows users to create a new grid up to 64x64.
---
+++ script.js
@@ -0,0 +1,18 @@
+/*const boxes = document.querySelectorAll('box');
+boxes.addEventListener('click', () => {
+  return 'background-color: red';
+});*/
+/*const boxes = document.querySelectorAll('box');
+const handleClick = () => {
+  return 'background-color: red';
+};
+
+boxes.forEach(box => {
+  box.addEventListener('click', handleClick);
+});*/
+
+function popUp() {
+    const popup= document.document.getElementById('popup');
+    popup.classList.toggle(show);
+  };
+  
